6222c284453dd2b9beca92df751f63f8e96e001e
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-02-07 22:28:02 +0500

    Builder

---
+++ Creational/Builder/Builder/HTML.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+var builder = new HtmlBuilder("ul")
+    .AddChild("li", "Sardor")
+    .AddChild("li", "Sarvar")
+    .AddChild("li", "Sanjar");
+
+Console.WriteLine(builder.ToString());
+//< ul >
+//    < li > Sardor </ li >
+//    < li > Sarvar </ li >
+//    < li > Sarjar </ li >
+//</ ul >
+
+class HtmlElement
+{
+    public string Name { get; }
+    public string Text { get; set; }
+    public List<HtmlElement> Children { get; } = new List<HtmlElement>();
+
+    public HtmlElement(string name, string text = "")
+    {
+        Name = name;
+        Text = text;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append($"<{Name}>{Text}");
+        foreach (var child in Children)
+        {
+            sb.Append(child.ToString());
+        }
+        sb.Append($"</{Name}>");
+        return sb.ToString();
+    }
+}
+
+class HtmlBuilder
+{
+    private HtmlElement root;
+
+    public HtmlBuilder(string rootName)
+    {
+        root = new HtmlElement(rootName);
+    }
+
+    public HtmlBuilder AddChild(string childName, string childText)
+    {
+        root.Children.Add(new HtmlElement(childName, childText));
+        return this;
+    }
+
+    public override string ToString() => root.ToString();
+}
---
+++ Creational/Builder/Builder/House.cs
@@ -0,0 +1,41 @@
+﻿/*var myHouse = House.Builder()
+                   .Rooms(4)
+                   .Bathrooms(2)
+                   .HasGarage(true)
+                   .HasGarden(false)
+                   .Build();
+
+Console.WriteLine($"Rooms: {myHouse.Rooms}, Bathrooms: {myHouse.Bathrooms}, Garage: {myHouse.HasGarage}, Garden: {myHouse.HasGarden}");
+*/
+public class House
+{
+    public int Rooms { get; }
+    public int Bathrooms { get; }
+    public bool HasGarage { get; }
+    public bool HasGarden { get; }
+
+    private House(int rooms, int bathrooms, bool hasGarage, bool hasGarden)
+    {
+        Rooms = rooms;
+        Bathrooms = bathrooms;
+        HasGarage = hasGarage;
+        HasGarden = hasGarden;
+    }
+
+    public static HouseBuilder Builder() => new HouseBuilder();
+
+    public class HouseBuilder
+    {
+        private int _rooms;
+        private int _bathrooms;
+        private bool _hasGarage;
+        private bool _hasGarden;
+
+        public HouseBuilder Rooms(int rooms) { _rooms = rooms; return this; }
+        public HouseBuilder Bathrooms(int bathrooms) { _bathrooms = bathrooms; return this; }
+        public HouseBuilder HasGarage(bool hasGarage) { _hasGarage = hasGarage; return this; }
+        public HouseBuilder HasGarden(bool hasGarden) { _hasGarden = hasGarden; return this; }
+
+        public House Build() => new House(_rooms, _bathrooms, _hasGarage, _hasGarden);
+    }
+}
