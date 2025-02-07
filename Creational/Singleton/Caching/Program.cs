ff3f79ae9aaad82635c5296d0ee51d8356c94d23
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-02-07 23:19:29 +0500

    Singleton

---
+++ Creational/Singleton/Caching/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caching
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var cache = SardorCache.Instance;
+            cache.Set("name", "Sardor");
+            cache.Set("age", "25");
+            Console.WriteLine(cache.Get("name"));
+            Console.WriteLine(cache.Get("age"));
+        }
+    }
+}
---
+++ Creational/Singleton/Caching/SardorCache.cs
@@ -0,0 +1,24 @@
+﻿public class SardorCache
+{
+    private static SardorCache _instance;
+    private Dictionary<string, string> _cache;
+
+    private SardorCache()
+    {
+        _cache = new Dictionary<string, string>();
+    }
+
+    public static SardorCache Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new SardorCache();
+            return _instance;
+        }
+    }
+
+    public string Get(string key) => _cache[key];
+
+    public void Set(string key, string value) => _cache[key] = value;
+}
