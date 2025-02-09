b9e4ff25e79cd2b91f3d84ba5daa9c843204ac7c
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-02-09 18:15:12 +0500

    3D

---
+++ Creational/Prototype/3D/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+
+public interface IPrototype
+{
+    object Clone();
+    object DeepClone();
+}
+
+public class Model3D<T> : IPrototype
+{
+    public string Name { get; set; }
+    public string Texture { get; set; }
+
+    public Model3D(string name, string texture)
+    {
+        Name = name;
+        Texture = texture;
+    }
+
+    public object Clone() 
+        => this.MemberwiseClone();
+
+    public object DeepClone()
+    {
+        string json = JsonSerializer.Serialize(this);
+        return JsonSerializer.Deserialize<T>(json);
+    }
+}
